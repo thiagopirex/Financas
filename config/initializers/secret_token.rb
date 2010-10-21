846397d82f48ac845e491c2814f13fe59c3319e1
thiagopirex <Thiago@.(none)>
2010-10-21 20:34:18 -0200

    Corrigindo commit da estrutura inicial do projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Financas::Application.config.secret_token = '05cc56a48ccaa64518269674ea02d4ca5746b3ff777551ad47f22dad9f070231d7098cca92f77c22afc2510e29dc108242d318b93de118b5f5442bf35130c4df'
