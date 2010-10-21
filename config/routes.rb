44e3d7d7ac6e927bcab9c8dc8aa0503a71eafae6
thiagopirex <Thiago@.(none)>
2010-10-21 21:20:33 -0200

    Alteracao da pagina inicial

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
-Financas::Application.routes.draw do
+﻿Financas::Application.routes.draw do
+  root :to => "home#index" 
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +50,7 @@ Financas::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  #root :to => "welcome#index"
 
   # See how all your routes lay out with "rake routes"
 
