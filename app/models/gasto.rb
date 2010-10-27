87936d0d2df1e995b8683d23653a744670153250
thiagopirex <Thiago@.(none)>
2010-10-27 18:11:45 -0200

    Inclusao de preenchimento obrigatorio para valor e descricao do gasto

--- app/models/gasto.rb
+++ app/models/gasto.rb
@@ -1,3 +1,13 @@
 ﻿class Gasto < ActiveRecord::Base
 
+  validates_presence_of :descricao, :message => "deve ser preenchido"
+  validates_presence_of :valor, :message => "deve ser preenchido"
+
+  validate :primeira_letra_descricao_maiuscula
+  
+  private
+  def primeira_letra_descricao_maiuscula
+    errors.add("Descrição", "deve ter a primeira letra maiúscula") unless descricao =~/[A-Z].*/
+  end
+  
 end
