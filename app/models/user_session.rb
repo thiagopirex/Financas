074739aa5bd22a4e0bee95118286d38b600e4b24
Joao Lucio Cardozo Espindola <joaolce@gmail.com>
2010-11-09 12:25:20 -0200

    alterado no dojo

--- app/models/user_session.rb
+++ app/models/user_session.rb
@@ -1,7 +1,7 @@
 ﻿class UserSession < Authlogic::Session::Base
 
-  def to_key
-    new_record? ? nil : [
-    self.send(self.class.primary_key) ]
-  end
-end
+def to_key
+  new_record? ? nil : [
+   self.send(self.class.primary_key) ]
+ end
+ end
