56d83fc6774a1db43c9cfd497837d3d6fed3f9ce
thiagopirex <Thiago@.(none)>
2010-11-10 22:51:21 -0200

    testes de gastos/usuario e associacao do gasto ao usuario logado

---
+++ db/migrate/20101110144206_add_user_to_gasto.rb
@@ -0,0 +1,20 @@
+class AddUserToGasto < ActiveRecord::Migration
+  def self.up
+     #Remove all gastos
+     execute "DELETE FROM gastos"
+     add_column :gastos, :user_id, :int, :null => false
+     
+     #add a foreign key
+     execute <<-SQL     
+       ALTER TABLE gastos
+         ADD CONSTRAINT fk_user
+         FOREIGN KEY(user_id)
+         REFERENCES users(id)
+     SQL
+  end
+
+  def self.down
+    execute "ALTER TABLE gastos DROP CONSTRAINT fk_user"
+    remove_column :gastos, :user_id
+  end
+end
